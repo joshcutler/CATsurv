8d57202ff303fd0cf81fc47a66ea5dd80b441ba8
mhoonkang <myunghoon@wustl.edu>
2014-03-24 00:04:15 -0500

    Update prior.R

--- CAT_KOW/prior.R
+++ CAT_KOW/prior.R
@@ -2,12 +2,12 @@
 #'
 #' This function returns the prior value for each repondent's position on the latent scale of interest. 
 #'
-#' @param cat an object of \code{CATsurv} class.
+#' @param cat an object of class \code{CATsurv}.
 #' @param values vector consisting of quantile for each respondent. 
 #' @param name character string for type of assumed distribution. \code{normal} for normal distribution, \code{cauchy} for Cauchy distribtuion, and \code{t} for t-distribution.
 #' @param params vector of parameters for selected distribution. The first element for mean value, the second element for standard deviation, and the third element for degree of freedom.
 #'
-#' @return vector of prior value for each respondent according to the assumed distribution.
+#' @return A vector consisting of prior value for each respondent according to the assumed distribution.
 #'  
 #' @author Josh W. Cutler and Jacob M. Montgomery
 #' @seealso \code{\link{three.pl}},\code{\link{likelihood}}, \code{\link{estimateTheta}}, \code{\link{estimateSE}}, \code{\link{expectedPV}}, \code{\link{nextItem}}, \code{\link{storeAnswer}}, \code{\link{debugNextItem}}
