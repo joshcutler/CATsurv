6aec274b001e3b159eb3823680dc1874e5878487
mhoonkang <myunghoon@wustl.edu>
2014-03-23 22:59:42 -0500

    Update prior.R

--- CAT_KOW/prior.R
+++ CAT_KOW/prior.R
@@ -1,17 +1,17 @@
-#' Title
+#' Computerized Adaptive Testing Survey Prior Value Function
 #'
-#' Definition
+#' This function returns the prior value for each repondent's position on the latent scale of interest. 
 #'
-#' @param cat 
-#' @param values
-#' @param name
-#' @param params
+#' @param cat an object of \code{CATsurv} class.
+#' @param values vector consisting of quantile for each respondent. 
+#' @param name character string for type of assumed distribution. \code{normal} for normal distribution, \code{cauchy} for Cauchy distribtuion, and \code{t} for t-distribution.
+#' @param params vector of parameters for selected distribution. The first element for mean value, the second element for standard deviation, and the third element for degree of freedom.
 #'
-#' @return An object of class CATsurv containing
-#'  \item{prior.value}{ }
+#' @return vector of prior value for each respondent according to the assumed distribution.
 #'  
 #' @author Josh W. Cutler and Jacob M. Montgomery
-
+#' @seealso \code{\link{three.pl}},\code{\link{likelihood}}, \code{\link{estimateTheta}}, \code{\link{estimateSE}}, \code{\link{expectedPV}}, \code{\link{nextItem}}, \code{\link{storeAnswer}}, \code{\link{debugNextItem}}
+#' @rdname prior.value
 #' @export
 setGeneric("prior", function(cat, values, name, params){standardGeneric("prior")})
 
