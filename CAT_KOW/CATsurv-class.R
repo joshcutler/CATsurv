21843a325b23be66657887c9748cfb7780591b23
warddalston <ward.dalston@gmail.com>
2014-03-24 15:11:25 -0500

    CATsurv-class
    Dalston writes documentation.

--- CAT_KOW/CATsurv-class.R
+++ CAT_KOW/CATsurv-class.R
@@ -1,23 +1,26 @@
-#' A CATsurv value object 
+#' A Computerized Adaptive Testing Survey (CATsurv) Object
 #' 
-#' Object of class \code{CATsurv} are created
+#' Objects of class \code{CATsurv} are used in administering Computerized Adaptive Testing (CAT) Surveys.  These objects contain several pieces of information relevent for CAT surveys, and are used as input in the main functions of the \code{CATsurv} package, \code{\link{nextItem}} and \code{\link{storeAnswer}}. They are created using the \code{initialize} function.      
 #'
-#' 
 #' An object of the class `CATsurv' has the following slots:
 #' \itemize{
-#' \item \code{questions}
-#' \item \code{difficulties} 
-#' \item \code{priorName}
-#' \item \code{priorParams}
-#' \item \code{poly}
-#' \item
+#' \item \code{questions} A data-frame where the rows represent the questions used in the CAT, and the columns contain information about each question.  See the details section for more information.  
+#' \item \code{difficulties} A named vector of difficulty parameters for use with polytomous questions.  Each element's name tells the question to which it applies.  This slot \emph{must} be filled if \code{poly=TRUE}.    
+#' \item \code{priorName} A character vector of length one giving the prior distribution to use for the latent trait estimates.  The options are \code{normal} for the normal distirbution, \code{cauchy} for the Cauchy distribution, are \code{t} for the t-distribution. Defaults to \code{normal}. 
+#' \item \code{priorParams} A numeric vector of parameters for the distribution specified in the \code{priorName} slot. See the details section for more infomration.  Defaults to \code{c(1,1)}.  
+#' \item \code{poly} A logical, indicating whether or not the questions using in this CAT have dichotomous (FALSE) or polytomous (TRUE) responses.  Defaults to FALSE. 
 #' }
 #'
+#'@details The "questions" data frame \emph{must} contain the following columns: "difficulty", which is a vector of difficulty parameters, one per question (unless poly=TRUE, in which case this column can be ommitted but the slot difficulties must be filled); "discrimination", which is a vector of discrimination parameters, also one per question; "guessing" which is a vector of guessing parameters; "answers" which is a vector of answers to questions as given by the survey respondent.\cr
+#' When priorName is set to "normal", the first priorParam is the mean, the second is the standard deviation.  When priorName is set to "Cauchy", the first priorParam is the location, and the second is the scale.  When priorName is set to "t", the first priorParam is mu, a location parameter, the second is sigma, a scale parameter, and the third is nu, the degrees of freedom parameter.   
+#'
 #' @author Josh W. Cutler: \email{josh@@zistle.com} and Jacob M. Montgomery: \email{jacob.montgomery@@wustl.edu}
+#' @seealso \code{\link{nextItem}}
+#' @seealso \code{\link{storeAnswer}}
 #' @aliases CATsurv-class initialize,CATsurv-method
 #' @rdname CATsurv
 #' @export
-library(sfsmisc)
+library(sfsmisc) ####PUT THIS AS A DEPENDS IN THE DESCRIPTION 
 
 class.name = "CATsurv"
 setClass(class.name,
