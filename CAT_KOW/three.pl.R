ece9ea705d58eb341ec7b1e1f3665a42668458b0
mhoonkang <myunghoon@wustl.edu>
2014-03-23 20:35:36 -0500

    Update three.pl.R
    Update three.pl.R

--- CAT_KOW/three.pl.R
+++ CAT_KOW/three.pl.R
@@ -1,19 +1,22 @@
-#' Title
+#' Three-Parameter IRT Model
 #'
-#' Definition
+#' This function returns the probability a correct response for each individual.
 #'
 #' @param cat 
-#' @param theta
-#' @param difficulty
-#' @param discrimination
-#' @param guessing
-#' @param D
+#' @param theta vector of each respondent's position on the latent scale of interest.
+#' @param difficulty an item difficulty parameter number.
+#' @param discrimination an item discrimination parameter number.
+#' @param guessing an item guessability parameter number. 
+#' @param D a model parameter number. 1 is for a logistic model and 1.702 for an approximation of the probit model. The defaulat value is 1. 
 #'
 #' @return An object of class CATsurv containing
-#'  \item{prob}{ }
-#'  
+#'  \item{prob}{The probability of a correct response for each individual}
+#'
+#' @details The probability of a correct response for respondent \eqn{j} is \deqn{Pr(y_j=1|\theta_j)=c+(1-c)\frac{\exp(Da(\theta_j-b))}{1+\exp(Da(\theta_j-b))}} where \theta_j is respondent \eqn{j}'s position on the latent scale of interest, \eqn{a} is an item discrimination parameter, \eqn{b} is an item difficulty parameter, and \eqn{c} is an item guessablity parameter.
+#'
 #' @author Josh W. Cutler and Jacob M. Montgomery
-
+#' @seealso \code{\link{likelihood}},\code{\link{prior}}, \code{\link{estimateTheta}}, \code{\link{estimateSE}}, \code{\link{expectedPV}}, \code{\link{nextItem}}, \code{\link{storeAnswer}}, \code{\link{debugNextItem}}
+#' @rdname three.pl
 #' @export
 setGeneric("three.pl", function(cat, theta, difficulty, discrimination, guessing, D=1){standardGeneric("three.pl")})
 
