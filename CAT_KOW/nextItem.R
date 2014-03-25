ab0767f96d62c30b7fde858b7a7cda5192c59576
eozdemir <eozdemir@wustl.edu>
2014-03-24 23:42:48 -0500

    Update nextItem.R
    Elif documents nextItem

--- CAT_KOW/nextItem.R
+++ CAT_KOW/nextItem.R
@@ -1,18 +1,19 @@
-#' Title
+#' Computerized Adaptive Testing Survey Next Item Function
 #'
-#' Definition
+#' This function takes a respondent's previous answers to determine the next item from the list of available questions, by using the expected \emph{a posteriori} (EAP) position on the latent scale. 
 #'
-#' @param cat
-#' @param theta.est 
-#' @param lowerBound
-#' @param upperBound
-#' @param quadPoints
+#' @param cat An object of class \code{CATsurv}
+#' @param theta.est A scalar value to contain an estimate of a respondent's position on the latent trait, using the \code{\link{estimateTheta}} funciton. Defaults to NA.
+#' @param D A numeric value used as model parameter.  For logit models, set D=1.  For an approximation of the probit model, set D=1.702.  Defaults to D=1.   
+#' @param lowerBound The lower bound of the interval of the latent trait used in estimation.  Defaults to -4.
+#' @param upperBound The upper bound of the interval of the latent trait used in estimation.  Defaults to 4.
+#' @param quadPoints The number of points used in approximating the integral.  Defaults to 33.
 #'
-#' @return An object of class CATsurv containing
-#'  \item{to.return}{ }
+#' @return A data frame of available questions based on the min expected posterior variance for respondent and a row name for the next item to be asked 
 #'  
-#' @author Josh W. Cutler and Jacob M. Montgomery
-
+#' @author Josh W. Cutler: \email{josh@@zistle.com} and Jacob M. Montgomery: \email{jacob.montgomery@@wustl.edu}
+#' @seealso \code{\link{three.pl}},\code{\link{likelihood}}, \code{\link{prior.value}}, \code{\link{estimateTheta}}, \code{\link{estimateSE}}, \code{\link{expectedPV}},  \code{\link{storeAnswer}}, \code{\link{debugNextItem}}
+#' @rdname nextItem
 #' @export
 setGeneric("nextItem", function(cat, theta.est=NA, D=1, lowerBound=-4, upperBound=4, quadPoints=33){standardGeneric("nextItem")})
 
