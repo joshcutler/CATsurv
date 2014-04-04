a1518740ba9c9761f8d7acb960783aa86d320e0d
mhoonkang <myunghoon@wustl.edu>
2014-04-04 11:16:23 -0500

    Update ltmCAT.R

--- CAT_KOW/CATpack Scripts/ltmCAT.R
+++ CAT_KOW/CATpack Scripts/ltmCAT.R
@@ -1,15 +1,15 @@
 #' Computerized Adaptive Testing Latent Trait Model with Binary Data
 #'
-#' This function fits the latent trait model for binary data and populates the fitted values for discimination and difficulty parameters to an object of class \code{DichoCATsurv}.  
+#' This function fits the latent trait model for binary data and populates the fitted values for discimination, difficulty, and guessing parameters to an object of class \code{CATsurv}.  
 #'
 #' @param data a \code{data.frame} or a numeric \code{matrix} of manifest variables. 
 #' @param object an object of class \code{CATsurv} to be populated. If omitted, a new object of class \code{CATsurv} is created.
-#' @param ... arguments to be passed to methods. For more details about the arguments, see \link{\code{ltm}}.
+#' @param ... arguments to be passed to methods. For more details about the arguments, see \link{\code{tpm}}.
 #'
 #'  @return An object of class \code{CATsurv} with components,
 #' \itemize{
 #' \item \code{difficulty} a named vector of difficulty parameters for use with dichotomous questions/items.  Each element's name tells the question/item to which it applies.
-#' \item \code{guessing} a vector of guessing parameters 
+#' \item \code{guessing} a vector of guessing parameters for each item.
 #' \item \code{discrimination} a vector of disrimination parameters including a numeric value per question/item.
 #' \item \code{answers} a vector of answers to questions as given by the survey respondent.
 #' \item \code{priorName} a character vector of length one giving the prior distribution to use for the latent trait estimates.  The options are \code{normal} for the normal distirbution, \code{cauchy} for the Cauchy distribution, are \code{t} for the t-distribution. Defaults to \code{normal}. 
@@ -24,17 +24,19 @@ setGeneric("ltmCAT", function(data, object=NULL, ...){standardGeneric("ltmCAT")}
 #' @export
 setMethod(f="ltmCAT", signature="data.frame", 
           definition=function(data, object,...){
-          if(!is.null(object)) if(class(object)!="CATsurv") stop("object is not class CATsurv")            
-          fit <- ltm(data~z1)
-          coefficient <- coef(fit)
-          discrimination <- coefficient[,"Dscrmn"]
-          difficulty <- coefficient[,"Dffclt"]
-          names(difficulty) <- rownames(coefficient)
-          if(is.null(object)){
-          return(new("CATsurv", discrimination=discrimination, difficulty=difficulty))
-          } else {
-          object@discrimination <- discrimination
-          object@difficulty <- difficulty
-          return(object)
-          }
-})
+            if(!is.null(object)) if(class(object)!="CATsurv") stop("object is not class CATsurv")            
+            fit <- tpm(data)
+            coefficient <- coef(fit)
+            discrimination <- coefficient[,"Dscrmn"]
+            difficulty <- coefficient[,"Dffclt"]
+            guessing <- coefficient[,"Gussng"]
+            names(difficulty) <- rownames(coefficient)
+            if(is.null(object)){
+              return(new("CATsurv", discrimination=discrimination, difficulty=difficulty, guessing=guessing))
+            } else {
+              object@discrimination <- discrimination
+              object@difficulty <- difficulty
+              object@guessing <- guessing
+              return(object)
+            }
+          })
