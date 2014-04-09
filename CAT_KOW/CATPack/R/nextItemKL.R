0c669c2e3e0128e1a37a5d8ef47467f9b0f1aee8
mhoonkang <myunghoon@wustl.edu>
2014-04-09 18:14:25 -0500

    Update nextItemKL.R

--- CAT_KOW/CATPack/R/nextItemKL.R
+++ CAT_KOW/CATPack/R/nextItemKL.R
@@ -12,17 +12,18 @@
 #' @seealso \code{\link{three.pl}},\code{\link{likelihood}}, \code{\link{prior.value}}, \code{\link{estimateTheta}}, \code{\link{estimateSE}}, \code{\link{expectedPV}},  \code{\link{storeAnswer}}, \code{\link{debugNextItem}}
 #' @rdname nextItemKL
 #' @export
-setGeneric("nextItemKL", function(cat, theta.est,...){standardGeneric("nextItemKL")})
+setGeneric("nextItemKL", function(cat){standardGeneric("nextItemKL")})
 
 #' @export
-setMethod(f="nextItemKL", signature="CATsurv", definition=function(cat, theta.est...) {
-  available_questions = data.frame(questions=which(is.na(cat@answers)),KL=NA)
+setMethod(f="nextItemKL", signature="CATsurv", definition=function(cat) {
+  colnames(available_questions) <- c("questions","KL")
+  
   num.asked <- sum(!is.na(cat@answers))
   
   delta.int <- seq(from=cat@lowerBound*(1-num.asked/length(cat@answers)), to=cat@upperBound*(1-num.asked/length(cat@answers)), length=cat@quadPoints)
   i=2
   for (i in 1:nrow(available_questions)) {
-    p.theta.hat <- three.pl(cat,theta.est,cat@difficulty[available_questions[i,1]], cat@discrimination[available_questions[i,1]], cat@guessing[available_questions[i,1]])
+    p.theta.hat <- three.pl(cat,cat@Theta.est,cat@difficulty[available_questions[i,1]], cat@discrimination[available_questions[i,1]], cat@guessing[available_questions[i,1]])
     
     p.theta.null <- three.pl(cat,delta.int,cat@difficulty[available_questions[i,1]], cat@discrimination[available_questions[i,1]], cat@guessing[available_questions[i,1]])
     
